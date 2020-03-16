ddbface450e89eab378f10526ba6137e96152b06
MrJc010 <32074443+MrJc010@users.noreply.github.com>
2020-03-16 10:47:19 -0700

    second push

---
+++ ShoopFloor_PhysicalReceiveService/src/main/java/com/shoopfloor/physicalreceiving/ShoopFloorPhysicalReceiveServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shoopfloor.physicalreceiving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoopFloorPhysicalReceiveServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoopFloorPhysicalReceiveServiceApplication.class, args);
+	}
+
+}
---
+++ ShoopFloor_PhysicalReceiveService/src/test/java/com/shoopfloor/physicalreceiving/ShoopFloorPhysicalReceiveServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoopfloor.physicalreceiving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoopFloorPhysicalReceiveServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
